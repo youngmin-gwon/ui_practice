799b48a6f8633d9aabab4d48ef0ec2fe7bea8afb
youngmin-gwon <youngmin.gwon@gmail.com>
2021-07-28 11:36:20 +0900

    add simple animations package usage

--- animation_practice/lib/main.dart
+++ animation_practice/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:animation_practice/animated_widget_page.dart';
 import 'package:animation_practice/basic_animation_page.dart';
+import 'package:animation_practice/stagger_demo_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,6 +8,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: BasicAnimatedWidget());
+    return MaterialApp(title: 'Material App', home: StaggerDemo());
   }
 }
