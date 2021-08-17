29ff2aa0fab15238d27df984b15983696e51356f
youngmin-gwon <youngmin.gwon@gmail.com>
2021-08-17 22:47:12 +0900

    [study] how to implement notification on flutter

---
+++ notification/lib/home_page.dart
@@ -0,0 +1,135 @@
+import 'dart:io';
+
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_awesome_notifications_tutorial/notifications.dart';
+import 'package:flutter_awesome_notifications_tutorial/plant_stats_page.dart';
+import 'package:flutter_awesome_notifications_tutorial/utilities.dart';
+import 'package:flutter_awesome_notifications_tutorial/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    // check if notification is allowed
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("Allow Notification"),
+            content: Text("Our app would lkie to send you notification"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  "Don't Allow",
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ),
+              ),
+              TextButton(
+                onPressed: () => AwesomeNotifications()
+                    .requestPermissionToSendNotifications()
+                    .then(
+                      (_) => Navigator.pop(context),
+                    ),
+                child: Text("Allow",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ],
+          ),
+        );
+      }
+    });
+
+    // set notification stream
+    AwesomeNotifications().createdStream.listen((notification) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text("Notification Created on ${notification.channelKey}")));
+    });
+
+    AwesomeNotifications().actionStream.listen((notification) {
+      if (notification.channelKey == "basic_channel" && Platform.isIOS) {
+        AwesomeNotifications().getGlobalBadgeCounter().then(
+              (value) =>
+                  AwesomeNotifications().setGlobalBadgeCounter(value - 1),
+            );
+      }
+
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (_) => PlantStatsPage()),
+        (route) => route.isFirst,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    AwesomeNotifications().actionSink.close();
+    AwesomeNotifications().createdSink.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: AppBarTitle(),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => PlantStatsPage(),
+                ),
+              );
+            },
+            icon: Icon(
+              Icons.insert_chart_outlined_rounded,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            PlantImage(),
+            SizedBox(
+              height: 25,
+            ),
+            HomePageButtons(
+              onPressedOne: createPlantFoodNotification,
+              onPressedTwo: () async {
+                NotificationWeekAndTime? pickedSchedule =
+                    await pickSchedule(context);
+
+                if (pickedSchedule != null) {
+                  createWaterReminderNotification(pickedSchedule);
+                }
+              },
+              onPressedThree: cancelScheduledNotification,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
