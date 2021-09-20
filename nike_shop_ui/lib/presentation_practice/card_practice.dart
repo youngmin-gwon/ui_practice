ee7a7572fbbc8eef1e139263767a956451366694
youngmin-gwon <youngmin.gwon@gmail.com>
2021-09-20 23:47:25 +0900

    [study] nike shop animation review

--- nike_shop_ui/lib/presentation_practice/card_practice.dart
+++ nike_shop_ui/lib/presentation_practice/card_practice.dart
@@ -2,13 +2,11 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/widgets.dart';
 
 import 'package:nike_shop_ui/domain/product.dart';
 
-class CardViewPractice extends StatefulWidget {
-  const CardViewPractice({
+class CardPractice extends StatefulWidget {
+  const CardPractice({
     Key? key,
     required this.product,
     required this.index,
@@ -29,63 +27,67 @@ class CardViewPractice extends StatefulWidget {
   final Alignment? alignment;
 
   @override
-  _CardViewPracticeState createState() => _CardViewPracticeState();
+  _CardPracticeState createState() => _CardPracticeState();
 }
 
-class _CardViewPracticeState extends State<CardViewPractice> {
-  double get _page => widget.page ?? 0;
-  int get _index => widget.index;
+class _CardPracticeState extends State<CardPractice> {
   Product get _product => widget.product;
-  Alignment get _alignment => widget.alignment ?? FractionalOffset.center;
+  int get _index => widget.index;
+  double get _page => widget.page ?? 0;
+
   double get _perspective => widget.perspective ?? 0.002;
   double get _angle => widget.angle ?? pi / 2;
-  double get _productAngle => widget.angle ?? -1;
+  double get _productAngle => widget.productAngle ?? -1;
+  Alignment get _alignment => widget.alignment ?? FractionalOffset.center;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        _alignment == const FractionalOffset(0.0, 0.5)
-            ? Transform(
-                transform: Matrix4.identity()
-                  ..setEntry(3, 2, _perspective)
-                  ..scale(1 - lerpDouble(0, 0.25, _index - _page)!)
-                  ..rotateY(_angle * (_page - _index)),
-                alignment: _alignment,
-                child: CardBackgroundPractice(product: _product),
-              )
-            : CardBackgroundPractice(product: _product),
-        Transform.translate(
-          offset: const Offset(0, -20),
-          child: Transform.rotate(
-            angle: _productAngle * (_page - _index),
-            child: SizedBox(
-              height: 160,
-              child: Center(
-                child: Transform.rotate(
-                  angle: 276,
-                  child: Image(
-                    image: _product.image,
-                  ),
+    return Stack(alignment: Alignment.center, children: [
+      _alignment == const FractionalOffset(0.0, 0.5)
+          ? Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, _perspective)
+                ..scale(1 - lerpDouble(0, -0.25, _index - _page)!)
+                ..rotateY(_angle * (_page - _index)),
+              alignment: _alignment,
+              child: CardPracticeBackground(product: _product),
+            )
+          : Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, _perspective)
+                ..scale(1 - lerpDouble(0, -0.25, _index - _page)!)
+                ..rotateY(_angle * (_page - _index)),
+              alignment: _alignment,
+              child: CardPracticeBackground(product: _product),
+            ),
+      Transform.translate(
+        offset: const Offset(0, -20),
+        child: Transform.rotate(
+          angle: _productAngle * (_page - _index),
+          child: SizedBox(
+            height: 160,
+            child: Center(
+              child: Transform.rotate(
+                angle: 276,
+                child: Image(
+                  image: _product.image,
                 ),
               ),
             ),
           ),
-        )
-      ],
-    );
+        ),
+      )
+    ]);
   }
 }
 
-class CardBackgroundPractice extends StatelessWidget {
-  const CardBackgroundPractice({
+class CardPracticeBackground extends StatelessWidget {
+  const CardPracticeBackground({
     Key? key,
-    required Product product,
-  })  : _product = product,
-        super(key: key);
+    required this.product,
+  }) : super(key: key);
 
-  final Product _product;
+  final Product product;
 
   @override
   Widget build(BuildContext context) {
@@ -96,78 +98,61 @@ class CardBackgroundPractice extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(16),
         clipBehavior: Clip.hardEdge,
-        child: Stack(children: [
-          Container(
-            height: 500,
-            color: Colors.white,
-          ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 32),
-              child: Text(
-                _product.title.toUpperCase(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1,
+        child: Stack(
+          children: [
+            Container(
+              height: 500,
+              color: Colors.white,
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: Text(
+                  product.title.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    letterSpacing: 2,
+                  ),
                 ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 32),
-                  child: Text(
-                    _product.subtitle.toUpperCase(),
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w300,
-                      letterSpacing: 2,
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 48,
+                color: Colors.black,
+                child: Row(children: [
+                  Expanded(
+                    child: Text(
+                      product.price.toUpperCase(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        letterSpacing: 1,
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 48,
-                  color: Colors.black,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Expanded(
-                        child: Text(
-                          _product.price.toUpperCase(),
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                            letterSpacing: 2,
-                          ),
-                        ),
+                  Opacity(
+                    opacity: 0.99,
+                    child: Container(
+                      width: 48,
+                      height: 48,
+                      color: const Color(0xFF232323),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white30,
                       ),
-                      Opacity(
-                        opacity: 0.99,
-                        child: Container(
-                            height: 48,
-                            width: 48,
-                            color: const Color(0xFF232323),
-                            child: const Icon(
-                              Icons.add,
-                              color: Colors.white30,
-                            )),
-                      )
-                    ],
-                  ),
-                )
-              ],
+                    ),
+                  )
+                ]),
+              ),
             ),
-          )
-        ]),
+          ],
+        ),
       ),
     );
   }
