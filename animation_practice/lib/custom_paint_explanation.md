d37ed48b2e6f497cc54f15c4f304aab9a5790e97
youngmin-gwon <youngmin.gwon@gmail.com>
2021-10-27 15:48:59 +0900

    [study] milk cup using custom painter

---
+++ animation_practice/lib/custom_paint_explanation.md
@@ -0,0 +1,6 @@
+# Custom Paint
+
+- 선, 다형체 등을 직접 그릴 수 있는 canvas object에 접근할 수 있게 해줌
+- dynamic하게 변화 시키고 싶은 widget이 있을 때 유용함(ex. 온도계 높이 조절하여 온도를 나타낼 때 등)
+- CustomPainter: 실제로 painting 하는 Widget
+- 비교적 간단한 애니메이션을 만들 때 유용함
---
+++ animation_practice/lib/milk_glass/milk_glass_screen.dart
@@ -0,0 +1,165 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MilkGlassScreen extends StatefulWidget {
+  const MilkGlassScreen({Key? key}) : super(key: key);
+
+  @override
+  _MilkGlassScreenState createState() => _MilkGlassScreenState();
+}
+
+class _MilkGlassScreenState extends State<MilkGlassScreen> {
+  double _skew = 0.2;
+  double _ratio = 0.7;
+  double _fullness = 0.5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 300,
+              height: 200,
+              child: CustomPaint(
+                painter: MilkGlassPainter(
+                  skew: .01 + _skew * .4,
+                  ratio: _ratio,
+                  fullness: _fullness,
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 24, top: 12),
+              child: const Text(
+                "Skewness",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Slider(
+              value: _skew,
+              onChanged: (double newValue) {
+                setState(() {
+                  _skew = newValue;
+                });
+              },
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 24, top: 12),
+              child: const Text(
+                "Ratio",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Slider(
+              value: _ratio,
+              onChanged: (double newValue) {
+                setState(() {
+                  _ratio = newValue;
+                });
+              },
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(left: 24, top: 12),
+              child: const Text(
+                "Fullness",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Slider(
+              value: _fullness,
+              onChanged: (double newValue) {
+                setState(() {
+                  _fullness = newValue;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MilkGlassPainter extends CustomPainter {
+  final double skew;
+  final double ratio;
+  final double fullness;
+  const MilkGlassPainter({
+    required this.skew,
+    required this.ratio,
+    required this.fullness,
+  });
+  @override
+  void paint(Canvas canvas, Size size) {
+    final glass = Paint()
+      ..color = Colors.white.withAlpha(150)
+      ..style = PaintingStyle.fill;
+    final milkTopColor = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    final milkColor = Paint()
+      ..color = const Color.fromARGB(255, 235, 235, 235)
+      ..style = PaintingStyle.fill;
+    final black = Paint()
+      ..color = Colors.black.withAlpha(150)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4;
+
+    final top = Rect.fromLTRB(
+      0,
+      0,
+      size.width,
+      size.width * skew,
+    );
+    final bottom = Rect.fromCenter(
+        center: Offset(
+          size.width * .5,
+          size.height - size.width * .5 * skew * ratio,
+        ),
+        width: size.width * ratio,
+        height: size.width * skew * ratio);
+
+    final liquidTop = Rect.lerp(top, bottom, fullness)!;
+
+    final cupPath = Path()
+      ..moveTo(top.left, top.top + top.height * .5)
+      ..arcTo(top, pi, pi, true)
+      ..lineTo(bottom.right, bottom.top + bottom.height / 2)
+      ..arcTo(bottom, 0, pi, true)
+      ..lineTo(top.left, top.top + top.height * .5);
+
+    final liquidPath = Path()
+      ..moveTo(liquidTop.left, liquidTop.top + liquidTop.height * .5)
+      ..arcTo(liquidTop, pi, pi, true)
+      ..lineTo(bottom.right, bottom.top + bottom.height / 2)
+      ..arcTo(bottom, 0, pi, true)
+      ..lineTo(liquidTop.left, liquidTop.top + liquidTop.height * .5);
+
+    canvas.drawPath(cupPath, glass);
+    canvas.drawPath(liquidPath, milkColor);
+    canvas.drawOval(liquidTop, milkTopColor);
+    canvas.drawPath(cupPath, black);
+    canvas.drawOval(top, black);
+  }
+
+  @override
+  bool shouldRepaint(covariant MilkGlassPainter oldDelegate) {
+    return oldDelegate.fullness != fullness ||
+        oldDelegate.ratio != ratio ||
+        oldDelegate.skew != skew;
+  }
+}
